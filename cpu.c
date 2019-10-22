ebf9ed50156e575778065116b21dc0b77cdfd93c
Oleksii Lytovchenko <oleksii.lytovchenko@gmail.com>
2019-10-22 23:30:21 +0300

    Implemented ADD A, r8 instruction
    - r8 here is an 8-bit register

--- cpu.c
+++ cpu.c
@@ -562,6 +562,13 @@ static inline ALWAYS_INLINE void cpu_opcode_daa() {
     cpu.f = SET_FLAGS(cpu.a == 0, n_flag, 0, set_carry_flag || GET_FLAG(C));
 }
 
+static inline ALWAYS_INLINE void cpu_opcode_add_a_r8(uint8_t r8) {
+    bool h_flag = (cpu.a & 0xF) <= (0xF - (r8 & 0xF));
+    bool c_flag = cpu.a <= (0xFF - r8);
+    cpu.a += r8;
+    SET_FLAGS(cpu.a == 0, 0, h_flag, c_flag);
+}
+
 // TODO: implement setting flags after steps
 static void cpu_step() {
     uint8_t instruction = read_byte(cpu.pc++);
@@ -992,20 +999,27 @@ static void cpu_step() {
             cpu.a = cpu.a;
             break;
         case 0x80: // ADD A, B
+            cpu_opcode_add_a_r8(cpu.b);
             break;
         case 0x81: // ADD A, C
+            cpu_opcode_add_a_r8(cpu.c);
             break;
         case 0x82: // ADD A, D
+            cpu_opcode_add_a_r8(cpu.d);
             break;
         case 0x83: // ADD A, E
+            cpu_opcode_add_a_r8(cpu.e);
             break;
         case 0x84: // ADD A, H
+            cpu_opcode_add_a_r8(cpu.h);
             break;
         case 0x85: // ADD A, L
+            cpu_opcode_add_a_r8(cpu.l);
             break;
         case 0x86: // ADD A, (HL)
             break;
         case 0x87: // ADD A, A
+            cpu_opcode_add_a_r8(cpu.a);
             break;
         case 0x88: // ADC A, B
             break;
